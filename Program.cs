321bf689398f123064b57d646c7b3b97e47743fd
Matheus Glauber <matheus.ribeiro13@fatec.sp.gov.br>
2020-04-22 18:58:37 -0300

    Fazendo versionamento para testar o git

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExFixacaoAula18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 550.50;
+            double medida = 53.234567;
+            double conta = Math.Pow(5, 2);
+
+            System.Console.WriteLine("Produtos:");
+            System.Console.WriteLine($"{produto1} cujo preco e ${preco1:F2}");
+            System.Console.WriteLine($"{produto2} cujo preco e ${preco2:F2}");
+            System.Console.WriteLine();
+            System.Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            System.Console.WriteLine();
+            System.Console.WriteLine($"Medida com oito casas decimais: {medida}");
+            System.Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            System.Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+            System.Console.WriteLine();
+            System.Console.WriteLine($"CONTA: {conta}");
+
+        }
+    }
+}
+
+namespace Testando{
+    class teste{
+        static string exibir(){
+            public ue;
+        }
+    }
+}
